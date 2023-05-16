afccfc1704597d48cb9777905017e2de0db18b88
la pyae <lapyaeoo@ayainnovation.com>
2023-05-16 09:55:37 +0630

    add: comments bloc

--- lib/src/bloc/comments_bloc.dart
+++ lib/src/bloc/comments_bloc.dart
@@ -1,3 +1,19 @@
-class CommentsBloc{
+import 'package:rxdart/rxdart.dart';
 
+import '../models/item_model.dart';
+
+class CommentsBloc {
+  final _commentsFetcher = PublishSubject<int>();
+  final _commentsOutput = BehaviorSubject<Map<int, ItemModel>>();
+
+  // Stream
+  Stream<Map<int, ItemModel>> get itemWithComment => _commentsOutput.stream;
+
+  // Sink
+  Function(int) get fetchItemWithComment => _commentsFetcher.sink.add;
+
+  dispose() {
+    _commentsFetcher.close();
+    _commentsOutput.close();
+  }
 }
