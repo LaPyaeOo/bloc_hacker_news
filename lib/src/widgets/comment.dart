79c14429786a5151fbd49d6686dc789a1b3ee2e8
la pyae <lapyaeoo@ayainnovation.com>
2023-06-09 00:56:17 +0630

    add: show all child comments and decoration

--- lib/src/widgets/comment.dart
+++ lib/src/widgets/comment.dart
@@ -13,11 +13,25 @@ class Comment extends StatelessWidget {
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: itemModelMap[itemId],
-      builder: (BuildContext context, AsyncSnapshot<ItemModel>snapshot) {
+      builder: (BuildContext context, AsyncSnapshot<ItemModel> snapshot) {
         if (!snapshot.hasData) {
           return const Text('Still Loading');
         }
-        return Text(snapshot.data!.text!);
+        final item = snapshot.data;
+        final children = <Widget>[
+          ListTile(
+            title: Text(item!.text!),
+            subtitle: Text(item.by!),
+          ),
+          const Divider(),
+        ];
+
+        item.kids!.map((kidsId) {
+          children.add(Comment(itemId: kidsId, itemModelMap: itemModelMap));
+        });
+        return Column(
+          children: children,
+        );
       },
     );
   }
